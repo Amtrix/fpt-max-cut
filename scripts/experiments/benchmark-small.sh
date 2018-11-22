8721efa12098c053ca9aba25afae345b5baea3dd
Amtrix <amtrix92@gmail.com>
2018-11-22 01:06:09 +0100

    Ops.

--- scripts/experiments/benchmark-small.sh
+++ scripts/experiments/benchmark-small.sh
@@ -18,10 +18,10 @@ do
         ./find-kernelization-general  -n $n -nc $nc -kernelization-efficiency -remove-iso \
                 -output-path ../data/output/experiments/kernelization/small/iso-rem/aggregate > ../data/output/experiments/kernelization/small/iso-rem/out-exe_n"$n"_c"$nc".txt
         ./find-kernelization-general  -n $n -nc $nc -kernelization-efficiency \
-                -output-path ../data/output/experiments/kernelization/small/iso-rem/aggregate > ../data/output/experiments/kernelization/small/iso-kept/out-exe_n"$n"_c"$nc".txt
+                -output-path ../data/output/experiments/kernelization/small/iso-kept/aggregate > ../data/output/experiments/kernelization/small/iso-kept/out-exe_n"$n"_c"$nc".txt
         ./find-kernelization-general  -n $n -nc $nc \
                 -output-path ../data/output/experiments/kernelization/small/iso-rem-no-kernelization/aggregate > ../data/output/experiments/kernelization/small/iso-rem-no-kernelization/out-exe_n"$n"_c"$nc".txt
         ./find-kernelization-general  -n $n -nc $nc \
-                -output-path ../data/output/experiments/kernelization/small/iso-rem-no-kernelization/aggregate > ../data/output/experiments/kernelization/small/iso-kept-no-kernelization/out-exe_n"$n"_c"$nc".txt
+                -output-path ../data/output/experiments/kernelization/small/iso-kept-no-kernelization/aggregate > ../data/output/experiments/kernelization/small/iso-kept-no-kernelization/out-exe_n"$n"_c"$nc".txt
     done
 done
