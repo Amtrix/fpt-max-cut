548e1d87310f88f9eac8ade323206d974541498d
Amtrix <amtrix92@gmail.com>
2018-11-23 03:26:14 +0100

    Missing data eval scripst

---
+++ data/output/experiments/linear-kernel/plotting.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+declare -a arr=("512" "2048")
+
+for i in "${arr[@]}"
+do
+
+    Rscript plotting.r --file n$i/out --out plot-n$i.pdf
+    Rscript plotting.r --file n$i/out --out plot-n$i-01.pdf --loess 0.1
+    Rscript plotting.r --file n$i/out --out plot-n$i-03.pdf --loess 0.3
+    Rscript plotting.r --file n$i/out --out plot-n$i-05.pdf --loess 0.5
+    Rscript plotting.r --file n$i/out --out plot-n$i-07.pdf --loess 0.7
+
+    Rscript plotting.r --file n$i/out --out plot-n$i-01-nop.pdf --loess 0.1 --nopoints
+    Rscript plotting.r --file n$i/out --out plot-n$i-03-nop.pdf --loess 0.3 --nopoints
+    Rscript plotting.r --file n$i/out --out plot-n$i-05-nop.pdf --loess 0.5 --nopoints
+    Rscript plotting.r --file n$i/out --out plot-n$i-07-nop.pdf --loess 0.7 --nopoints
+done
