1cb1ea13ec45c84eb540b0f353d77db6eafb5a78
Amtrix <amtrix92@gmail.com>
2019-02-16 02:46:46 +0100

    Update

--- scripts/experiments/solvers-realworld-small.sh
+++ scripts/experiments/solvers-realworld-small.sh
@@ -13,7 +13,6 @@ func_localize() {
     #10 hours:
     allowed_total_time_seconds=36000
 
-    allowed_total_time_seconds=600
 
     mkdir -p $experiment_outdir/solvers/real-world-small/
     $builddir/./$selected_build -action "kernelization" -iterations 1 -fdir $thesis_tests/real-world-small-missing \
